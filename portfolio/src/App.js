fddb0c32d2891b806e8e2d8c8e4932f88baf766e
saikiranambati942 <saikiranambati942@gmail.com>
2020-10-10 21:02:35 +0200

    added reactjs portfolio app

---
+++ portfolio/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+        this.toggleBioDisplay=this.toggleBioDisplay.bind(this);
+    }
+
+    toggleBioDisplay(){
+        this.setState({displayBio:!this.state.displayBio})
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Sai</p>
+                <p>I'm a Software Engineer and I'm always looking forward to work on meaningful projects</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Sweden and code every day</p>
+                            <p>My favorite language is Javascript and ReactJS is awesome</p>
+                            <p>Besides coding, I listen to music</p>
+                            <button onClick= {this.toggleBioDisplay}>Show less</button>
+                        </div>
+                    ) : (
+                    <div>
+                      <button  onClick = {this.toggleBioDisplay}>Read more</button>
+                    </div>
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+export default App;
