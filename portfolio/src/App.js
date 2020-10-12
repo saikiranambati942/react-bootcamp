0325362dc15574ce68c6daec5bca043d8be64c2f
saikiranambati942 <saikiranambati942@gmail.com>
2020-10-12 20:25:05 +0200

    added project component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Projects from './Projects';
 
 
 class App extends Component {
@@ -32,6 +33,8 @@ class App extends Component {
                     </div>
                     )
                 }
+                <hr />
+                <Projects />
             </div>
         )
     }
