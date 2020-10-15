71c0feb1e2a4f38ef24becce1c4bcf4999076f0f
saikiranambati942 <saikiranambati942@gmail.com>
2020-10-15 20:26:34 +0200

    worked with props topic

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+class Project extends Component {
+
+    render() {
+        console.log('this.props', this.props);
+        const { title, image, description, link } = this.props.project;
+        // const title=this.props.project.title;
+        // const image=this.props.project.image;
+        // const description=this.props.project.description;
+        // const link=this.props.project.link;
+
+        return (
+            <div style={{display:'inline-block', width: 500, margin: 10}}>
+                <h3>{title}</h3>
+                <img src={image} alt='profile' style={{width: 350, height: 200}}/>
+                <p> {description}</p>
+                <a href={link}>{link}</a>
+            </div>
+        )
+    }
+}
+
+
+
 
 class Projects extends Component {
-
     render() {
         return (
             <div>
@@ -12,7 +34,7 @@ class Projects extends Component {
                     {
                         PROJECTS.map(PROJECT => {
                             return (
-                                <div key={PROJECT.id}>{PROJECT.title}</div>
+                                <Project key={PROJECT.id} project={PROJECT} />
                             );
                         })
                     }
