7c68241356d81f59092fd0e9e68594a271fbe80c
saikiranambati942 <saikiranambati942@gmail.com>
2020-10-23 22:58:09 +0200

    Updated the Jokes component

--- portfolio/src/components/Jokes.js
+++ portfolio/src/components/Jokes.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 
+const Joke=({joke})=>{
+    const { setup, punchline } = joke;
+    return <p style={{margin:20}}>{setup}<em>{punchline}</em> </p>
+}
+
 class Jokes extends Component {
-    state = { joke: {} };
-    
+    state = { joke: {}, jokes:[] };
+
     componentDidMount() {
         fetch('https://official-joke-api.appspot.com/random_joke')
             .then(response => response.json())
             .then(json => this.setState({ joke: json }));
+
+    }
+    fetchJokes=()=>{
+        fetch('https://official-joke-api.appspot.com/random_ten')
+        .then(response=>response.json())
+        .then(json => this.setState({jokes:json}))
     }
 
     render() {
-        const { setup, punchline } = this.state.joke;
+        
         return (
             <div>
                 <h2>Highlighted jokes</h2>
-                <p>{setup} <em>{punchline}</em></p>
+               <Joke joke={this.state.joke}/>
+                <hr/>
+                <h3>Want ten new jokes?</h3>
+                <button  onClick = {this.fetchJokes}>Click Me!</button>
+                {
+                    this.state.jokes.map(joke => (<Joke key={joke.id} joke={joke}/>))
+                }
             </div>
         )
     }
