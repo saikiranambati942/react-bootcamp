1ba6daa29847d6aa4148232fa13767a05956717e
saikiranambati942 <saikiranambati942@gmail.com>
2020-10-23 21:46:46 +0200

    added Jokes component

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -3,6 +3,7 @@ import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
 import profile from '../assets/profile.jpeg';
 import Title from './Title';
+import Jokes from './Jokes';
 
 class App extends Component {
     state={displayBio:false};
@@ -41,6 +42,8 @@ class App extends Component {
                 <Projects />
                 <hr />
                 <SocialProfiles />
+                <hr/>
+                <Jokes/>
             </div>
         )
     }
