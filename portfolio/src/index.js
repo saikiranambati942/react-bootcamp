da5b6ca073088707ea4225c1aa00a5daf808c489
saikiranambati942 <saikiranambati942@gmail.com>
2020-10-24 23:28:44 +0200

    learnt promises in react

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -8,3 +8,26 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+// new Promise( (resolve)=>{
+
+//     setTimeout( () =>{
+//         resolve('Hello world!')
+//     },2000);
+
+// })
+// .then(quote => {
+//     console.log(quote);
+// })
+
+
+// new Promise( (resolve,reject)=>{
+
+//     return reject(new Error('Error occurred'));
+//     setTimeout( () =>{
+//         resolve('Hello world!')
+//     },2000);
+// })
+// .then(quote => {
+//     console.log(quote);
+// })
+// .catch(error=>console.log(error))
